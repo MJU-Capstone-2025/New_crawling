e97afd9bb44246614fc3ca54728185eb8ecf6dfd
sjinie <sjinie98@gmail.com>
2025-05-23 19:36:56 +0900

    코드 통합, 오탈자 수정, 추후 마저 통합할 예정

--- crawling_dailycoffeenews.py
+++ crawling_dailycoffeenews.py
@@ -9,16 +9,15 @@ from dateutil.parser import parse
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-today_str = datetime.now().strftime('%y%m%d')
-filename = f'./data/dailycoffeenews_{today_str}.csv'
-
-if os.path.exists(filename):
-    print(f"[중단] 오늘({today_str}) 파일이 이미 존재합니다. 수집을 건너뜁니다.")
-    exit()
-
 # 날짜 범위 설정
 start_date = datetime(2015, 1, 1)
 end_date = datetime(2025, 3, 31)
+
+filename = f'./data/dailycoffeenews_{end_date.strftime('%y%m%d')}.csv'
+
+if os.path.exists(filename):
+    print(f"[중단] {filename} 파일이 이미 존재합니다. 수집을 건너뜁니다.")
+    exit()
 
 # 크롬 드라이버 설정
 chrome_options = Options()
